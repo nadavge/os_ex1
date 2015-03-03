ff2e091dd56bb95a6f6b9564df7a41d63e4bea96
TheAmazingRabbit <danondaniel@gmail.com>
2015-03-03 13:55:08 +0200

    added defines

--- osm.c
+++ osm.c
@@ -1,24 +1,24 @@
 #include "osm.h"
 
-
+#define DEFAULT_ITERATIONS 50000
 // timeMeasurmentStructure
 // OSM_NULLSYSCALL
 
 /* Initialization function that the user must call
  * before running any other library function.
- * Returns 0 uppon success and -1 on failure.
+ * Returns 0 upon success and -1 on failure.
  */
 int osm_init();
 
 /* Time measurement function for an empty function call.
-   returns time in nano-seconds upon success, 
+   returns time in nano-seconds upon success,
    and -1 upon failure.
    Zero iterations number is invalid.
    */
 double osm_function_time(unsigned int osm_iterations);
 
 /* Time measurement function for an empty trap into the operating system.
-   returns time in nano-seconds upon success, 
+   returns time in nano-seconds upon success,
    and -1 upon failure.
    Zero iterations number is invalid.
 
