5a65598c769acec43a6eb80e3a14b3d799770ffc
TheAmazingRabbit <danondaniel@gmail.com>
2015-03-03 14:04:29 +0200

    Finished the main

--- osm.c
+++ osm.c
@@ -1,4 +1,5 @@
 #include "osm.h"
+#include <unistd.h>
 
 #define DEFAULT_ITERATIONS 50000
 // timeMeasurmentStructure
@@ -8,7 +9,10 @@
  * before running any other library function.
  * Returns 0 upon success and -1 on failure.
  */
-int osm_init();
+int osm_init()
+{
+
+}
 
 /* Time measurement function for an empty function call.
    returns time in nano-seconds upon success,
@@ -32,5 +36,31 @@ double osm_syscall_time(unsigned int osm_iterations);
    Zero iterations number is invalid.
    */
 double osm_operation_time(unsigned int osm_iterations);
+/*
+typedef struct {
+	char machineName[HOST_NAME_MAX];
+	int numberOfIterations;
+	double instructionTimeNanoSecond;
+	double functionTimeNanoSecond;
+	double trapTimeNanoSecond;
+	double functionInstructionRatio;
+	double trapInstructionRatio;
+} timeMeasurmentStructure;
+*/
+timeMeasurmentStructure measureTimes (unsigned int osm_iterations)
+{
+    timeMeasurmentStructure result = {0};
+    if(! gethostname(result.machineName, HOST_NAME_MAX))
+    {
+        result.machineName[0] = NULL;
+    }
+    result.numberOfIterations = osm_iterations > 0 ? osm_iterations : DEFAULT_ITERATIONS;
+    result.instructionTimeNanoSecond = osm_syscall_time(result.numberOfIterations);
+    result.functionTimeNanoSecond = osm_function_time(result.numberOfIterations);
+    result.trapTimeNanoSecond = osm_syscall_time(result.numberOfIterations);
+    result.functionInstructionRatio = result.functionTimeNanoSecond / result.instructionTimeNanoSecond;
+    result.trapInstructionRatio = result.trapTimeNanoSecond / result.instructionTimeNanoSecond;
 
-timeMeasurmentStructure measureTimes (unsigned int osm_iterations);
+
+
+}
